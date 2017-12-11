b1e5a3bbbda5879b511449f2a9331f6e0c27bde2
Rodmendoza <lr.mendozar@gmail.com>
2017-12-10 18:10:35 -0600

    fin del tuto

--- src/models/user.js
+++ src/models/user.js
@@ -42,4 +42,57 @@ userModel.insertUser = (userData, callback) => {
     }
 };
 
+/** metodo para actualizar usuario */
+
+userModel.updateUser = (userData, callback) => {
+    if (connection) {
+        
+        const sql = `
+            UPDATE users SET
+            username = ${connection.escape(userData.username)}
+            , passwd = ${connection.escape(userData.passwd)}
+            , email = ${connection.escape(userData.email)}
+            WHERE id = ${connection.escape(userData.id)}
+        `
+
+        connection.query(sql, (err, result) => {
+            if (err) {
+                throw err;
+            } else {
+                callback(null, {
+                    msg: "success"
+                })
+            }
+        })
+    }
+};
+
+userModel.deleteUser = (id, callback) => {
+    if (connection) {
+        let sql = `
+            SELECT * FROM users WHERE id = ${connection.escape(id)}
+        `;
+        connection.query(sql, (err, row) => {
+            if (row) {
+                let sql = `
+                    DELETE FROM users WHERE id = ${connection.escape(id)}
+                `;
+                connection.query(sql, (err, result) =>{
+                    if (err) {
+                        throw err;
+                    } else {
+                        callback(null, {
+                            msg: "Usuario eliminado"
+                        });
+                    }
+                });
+            } else {
+                callback(null, {
+                    msg:"no existe el usuario"
+                });
+            }
+        });
+    }
+};
+
 module.exports = userModel;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,71 @@
 const User = require('../models/user');
 
 module.exports = function(app) {
-    app.get('/', (req, res) => {
+    app.get('/users', (req, res) => {
         User.getUsers((err, data) => {
             res.status(200).json(data);
         });
     });
 
-    app.post('/users')
+    app.post('/users', (req, res) => {
+        const userData = {
+            id: null,
+            username: req.body.username,
+            passwd: req.body.passwd,
+            email: req.body.email,
+            created_at: null,
+            update_at: null
+        };
+        User.insertUser(userData, (err, data) => {
+            if (data && data.InsertId) {
+                console.log(data);
+                res.json({
+                    success: true,
+                    msg: "Datos insertados",
+                    data: data
+                })
+            } else {
+                res.status(500).json({
+                    success: false,
+                    msg: "Error"
+                });
+            }
+        });
+    });
+
+    app.put('/users', (req, res) => {
+        const userData = {
+            id: req.body.id,
+            username: req.body.username,
+            passwd: req.body.passwd,
+            email: req.body.email,
+            created_at: null,
+            update_at: null
+        };
+        User.updateUser(userData, (err, data) => {
+            if (data && data.msg) {
+                res.json(data)
+            } else {
+                res.json({
+                    success: false,
+                    msg: "Error"
+                })
+            }
+        })
+    });
+
+    app.delete('/users/:id',(req, res) => {
+        User.deleteUser(req.params.id, (err, data) => {
+            if (data && (data.msg == "Usuario eliminado" || data.msg == "no existe el usuario")) {
+                res.json({
+                    success: true,
+                    msg: data.msg
+                })
+            } else {
+                res.status(500).json({
+                    msg: "Error"
+                })
+            }
+        });
+    });
 }
